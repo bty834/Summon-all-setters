387d254dd659081c3d8b5614675177e2c94f63e1
GitHub Action <action@github.com>
2024-05-24 13:01:54 +0000

    Template cleanup

--- src/main/kotlin/com/github/bty834/pddprogressbar/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/bty834/pddprogressbar/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.bty834.pddprogressbar.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
